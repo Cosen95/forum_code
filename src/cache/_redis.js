309523aa2695d05e18a67e3c2e425b6d26860508
fengshuan <1263215592@qq.com>
2020-01-09 16:37:43 +0800

    :sparkles: redis初始化

---
+++ src/cache/_redis.js
@@ -0,0 +1,8 @@
+const redis = require("redis");
+const { REDIS_CONF } = require("../config/db.js");
+
+// 创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+redisClient.on("error", err => {
+  console.error("redis error", err);
+});
---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+/**
+ * @description 配置相关
+ */
+let REDIS_CONF = {
+  port: 6379,
+  host: "127.0.0.1"
+};
+
+module.exports = {
+  REDIS_CONF
+};
