08208c3d408e4e2d090f4d9a3f8625dc61ee42e8
fengshuan <1263215592@qq.com>
2020-01-09 17:22:07 +0800

    :memo: session适用场景

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -1,6 +1,21 @@
 /**
  * @description redis相关配置 get set
  */
+
+// 解决方案
+// 将web server和redis拆分为两个单独的服务
+// 双方都是独立的，都是可扩展的（例如都扩展成集群）
+// mysql也是一个单独的服务，也可扩展
+
+// 为什么session适合用redis?
+// session访问频繁，对性能要求极高
+// session可不考虑断电数据丢失问题
+// session数据量不会太大
+
+// 为什么网站数据不适合用redis?
+// 操作频率不是太高
+// 断电断电不能丢失，必须保留
+// 数据量太大，内存成本高
 const redis = require("redis");
 const { REDIS_CONF } = require("../config/db.js");
 
