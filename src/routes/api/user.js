7ce1ea0c7afda373adb27ec847bcdc9d706b33ef
fengshuan <1263215592@qq.com>
2020-01-14 23:15:38 +0800

    :sparkles: 创建用户api路由

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,9 +1,16 @@
 const router = require("koa-router")();
-const { isExist } = require("../../controller/user");
+const { isExist, register } = require("../../controller/user");
 router.prefix("/api/user");
 
 // 注册
-// router.post("/register", async (ctx, next) => {});
+router.post("/register", async ctx => {
+  const { userName, password, gender } = ctx.request.body;
+  ctx.body = await register({
+    userName,
+    password,
+    gender
+  });
+});
 
 // 用户名是否存在
 router.post("/isExist", async ctx => {
