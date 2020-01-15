19896f3805fc5bde7a0660b2aca6d83c3b8b0467
fengshuan <1263215592@qq.com>
2020-01-15 22:13:37 +0800

    :sparkles: 获取登录信息

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,12 +4,28 @@
 
 const router = require("koa-router")();
 
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false // 默认未登录
+  };
+
+  const userInfo = ctx.session.userInfo;
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    };
+  }
+
+  return data;
+}
+
 router.get("/login", async ctx => {
-  await ctx.render("login", {});
+  await ctx.render("login", getLoginInfo(ctx));
 });
 
 router.get("/register", async ctx => {
-  await ctx.render("register", {});
+  await ctx.render("register", getLoginInfo(ctx));
 });
 
 module.exports = router;
