47ebe016f94b6cd37c533393e8f96bd7b9c7e22a
fengshuan <1263215592@qq.com>
2020-01-20 16:14:57 +0800

    :white_check_mark: 用户更新模块接口测试

--- test/user/api.test.js
+++ test/user/api.test.js
@@ -54,9 +54,40 @@ test("登录，应该成功", async () => {
   COOKIE = res.headers["set-cookie"].join(";");
 });
 
+// 修改用户信息
+test("修改用户信息，可以成功", async () => {
+  const res = await server
+    .patch("/api/user/changeInfo")
+    .send({
+      nickName: "测试昵称",
+      city: "测试城市",
+      picture: "/test.jpg"
+    })
+    .set("cookie", COOKIE);
+  expect(res.body.errno).toBe(0);
+});
+
+// 修改密码
+test("修改密码应该成功", async () => {
+  const res = await server
+    .patch("/api/user/changePassword")
+    .send({
+      password,
+      newPassword: "password_test1"
+    })
+    .set("cookie", COOKIE);
+  expect(res.body.errno).toBe(0);
+});
+
 // 删除
 test("删除用户，应该成功", async () => {
   const res = await server.post("/api/user/delete").set("cookie", COOKIE);
+  expect(res.body.errno).toBe(0);
+});
+
+// 退出
+test("退出登录应该成功", async () => {
+  const res = await server.post("/api/user/logout").set("cookie", COOKIE);
   expect(res.body.errno).toBe(0);
 });
 
