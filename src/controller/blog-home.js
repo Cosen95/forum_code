5788e3139ba4919af6d0fa6c5fa0203f36b8d0d9
fengshuan <1263215592@qq.com>
2020-01-26 14:37:38 +0800

    :lock: 防止创建博客xss攻击

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -5,11 +5,12 @@
 const { createBlog } = require("../services/blog");
 const { SuccessModel, ErrorModel } = require("../model/ResModel");
 const { createBlogFailInfo } = require("../model/ErrorInfo");
+const xss = require("xss");
 async function create({ userId, content, image }) {
   try {
     const blog = await createBlog({
       userId,
-      content,
+      content: xss(content),
       image
     });
     return new SuccessModel(blog);
