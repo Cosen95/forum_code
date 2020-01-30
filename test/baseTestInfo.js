0583276e16f995dd88d65df2f53ce543a5911bfe
fengshuan <1263215592@qq.com>
2020-01-30 21:17:01 +0800

    :white_check_mark: 广场页单元测试

--- test/baseTestInfo.js
+++ test/baseTestInfo.js
@@ -1,5 +1,5 @@
 module.exports = {
   COOKIE:
-    "forum.sid=Y3N3VNGulkyCoesr6EWCgpMyxsZ2fi4w; forum.sid.sig=5OIsklVYXmc6d2JiJ7aoR9LRNzs",
+    "forum.sid=ShAon7JoAPEDdTcgsUrkRdhT-EwvF5bO; forum.sid.sig=eY6ZatSY3UoFiTfjiAZnplZt670",
   USER_NAME: "fs"
 };
