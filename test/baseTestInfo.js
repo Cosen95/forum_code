aec0c2ff7c157c4566e258ba00c9f0c105931f9f
fengshuan <1263215592@qq.com>
2020-02-01 16:57:14 +0800

    :white_check_mark: 关注/取消关注单元测试

--- test/baseTestInfo.js
+++ test/baseTestInfo.js
@@ -1,5 +1,11 @@
 module.exports = {
+  curID: 1,
   COOKIE:
     "forum.sid=ShAon7JoAPEDdTcgsUrkRdhT-EwvF5bO; forum.sid.sig=eY6ZatSY3UoFiTfjiAZnplZt670",
-  USER_NAME: "fs"
+  USER_NAME: "forest",
+
+  otherId: 2,
+  OTHER_COOKIE:
+    "forum.sid=66czB49RFrRfeHzWyBuMENxjdEmqrEPE; forum.sid.sig=RIlh_aBM8J4L_kxUe7sqw7Q42do",
+  OTHER_USER_NAME: "zhichen"
 };
---
+++ test/user/relation.test.js
@@ -0,0 +1,58 @@
+const server = require("../server");
+const { getFans, getFollowers } = require("../../src/controller/user-relation");
+
+const {
+  curID,
+  COOKIE,
+  USER_NAME,
+  otherId,
+  OTHER_COOKIE,
+  OTHER_USER_NAME
+} = require("../baseTestInfo");
+
+// 当前登录用户（森林）先取消关注另一个用户（之晨）
+test("先取消关注", async () => {
+  const res = await server
+    .post("/api/profile/unFollow")
+    .send({ userId: otherId })
+    .set("cookie", COOKIE);
+  expect(1).toBe(1);
+});
+
+test("森林关注之晨，应该可以成功", async () => {
+  const res = await server
+    .post("/api/profile/follow")
+    .send({ userId: otherId })
+    .set("cookie", COOKIE);
+  expect(res.body.errno).toBe(0);
+});
+
+test("获取之晨的粉丝，应该有森林", async () => {
+  const result = await getFans(otherId);
+  const { count, fansList } = result.data;
+  const hasFans = fansList.some(item => {
+    return item.userName === USER_NAME;
+  });
+
+  expect(count > 0).toBe(true);
+  expect(hasFans).toBe(true);
+});
+
+test("获取森林的关注人，应该有之晨", async () => {
+  const result = await getFollowers(curID);
+  const { count, followersList } = result.data;
+  const hasFollowed = followersList.some(item => {
+    return item.userName === OTHER_USER_NAME;
+  });
+
+  expect(count > 0).toBe(true);
+  expect(hasFollowed).toBe(true);
+});
+
+test("森林取消关注之晨，应该可以成功", async () => {
+  const res = await server
+    .post("/api/profile/unFollow")
+    .send({ userId: otherId })
+    .set("cookie", COOKIE);
+  expect(res.body.errno).toBe(0);
+});
