1b45ba07817df2fac7c17f843fd827b900cd4269
fengshuan <1263215592@qq.com>
2020-02-02 21:03:51 +0800

    :sparkles: 首页右侧模板/api

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -10,7 +10,31 @@ const { isExist } = require("../../controller/user");
 const { getFans, getFollowers } = require("../../controller/user-relation");
 // 首页
 router.get("/", loginRedirect, async ctx => {
-  await ctx.render("index", {});
+  const userInfo = ctx.session.userInfo;
+  const { id: userId } = userInfo;
+
+  // 获取第一页数据
+
+  // 获取粉丝
+  const fansResult = await getFans(userId);
+  const { count: fansCount, fansList } = fansResult.data;
+
+  // 获取关注人列表
+  const followersResult = await getFollowers(userId);
+  const { count: followersCount, followersList } = followersResult.data;
+  await ctx.render("index", {
+    userData: {
+      userInfo,
+      fansData: {
+        count: fansCount,
+        list: fansList
+      },
+      followersData: {
+        count: followersCount,
+        list: followersList
+      }
+    }
+  });
 });
 // 个人主页
 router.get("/profile", loginRedirect, async ctx => {
